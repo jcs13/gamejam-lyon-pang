9b29bdd9278ca8d89ded3581ff2ddd00024b0af9
Julien <julien.ciais@orange.com>
2015-09-29 16:07:08 +0200

    add tuto

--- WebContent/gamejam-pang.js
+++ WebContent/gamejam-pang.js
@@ -81,7 +81,7 @@ function create() {
     aliens.enableBody = true;
     aliens.physicsBodyType = Phaser.Physics.ARCADE;
 
-    createAliens();
+    createAliens();ldBound
 
     //  The score
     scoreString = 'Score : ';
